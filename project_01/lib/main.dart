f781fbc3226e5bb8067926c39b8ab7d16c7bac03
murali kumar <muraliofficialid@gmail.com>
2022-07-20 17:53:13 +0530

    bloc observer added

--- project_01/lib/main.dart
+++ project_01/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_01/src/blocs/app_bloc_observer.dart';
 import 'package:project_01/src/myapp.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () {
+      runApp(const MyApp());
+    },
+    blocObserver: AppBlocObserver(),
+  );
 }
